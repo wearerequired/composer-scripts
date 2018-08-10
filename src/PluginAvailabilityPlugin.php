4da5e550e2e88ac28d7cd135ac1a19e24921d884
Pascal Birchler <hello@pascalbirchler.ch>
2018-08-10 14:33:04 +0200

    Compare 'Tested up to' data for plugins
    See #13.

--- src/PluginAvailabilityPlugin.php
+++ src/PluginAvailabilityPlugin.php
@@ -73,6 +73,16 @@ class PluginAvailabilityPlugin implements PluginInterface, EventSubscriberInterf
 	public function checkMaintenanceStatus( PackageEvent $event ) {
 		$package = $this->getPackage( $event );
 
+		if ( $this->helper->isWordPressPlugin( $package ) && ! $this->hasPluginBeenTestedWithLatestVersions( $this->helper->getPluginApiURL( $package ) ) ) {
+			$this->io->writeError( sprintf(
+				'<warning>The plugin %s has not been tested with the latest 3 major releases of WordPress. Please double-check before using it.</warning>',
+				$package->getName()
+			) );
+
+			// We don't want to display two warnings for the same plugin.
+			return;
+		}
+
 		if ( $this->helper->isWordPressPlugin( $package ) && ! $this->isPluginActivelyMaintained( $this->helper->getPluginApiURL( $package ) ) ) {
 			$this->io->writeError( sprintf(
 				'<warning>The plugin %s has not been updated in over two years. Please double-check before using it.</warning>',
@@ -101,13 +111,9 @@ class PluginAvailabilityPlugin implements PluginInterface, EventSubscriberInterf
 	 * @return bool True if the plugin is available, false if otherwise (404 status).
 	 */
 	protected function isPluginAvailable( $url ) {
-		try {
-			$result = $this->loadPluginData( $url );
+		$result = $this->loadPluginData( $url );
 
-			return null !== $result;
-		} catch ( ErrorException $e ) {
-			return false;
-		}
+		return null !== $result;
 	}
 
 	/**
@@ -118,20 +124,85 @@ class PluginAvailabilityPlugin implements PluginInterface, EventSubscriberInterf
 	 * @return bool True if the plugin has been updated in the last two years, false otherwise.
 	 */
 	protected function isPluginActivelyMaintained( $url ) {
-		try {
-			$result = $this->loadPluginData( $url );
+		$result = $this->loadPluginData( $url );
 
-			if ( null === $result ) {
-				return false;
-			}
-
-			$now          = new DateTime();
-			$last_updated = new DateTime( $result['last_updated'] );
-
-			return $last_updated > $now->modify( '-2 years' );
-		} catch ( ErrorException $e ) {
+		if ( null === $result ) {
 			return false;
 		}
+
+		$now          = new DateTime();
+		$last_updated = new DateTime( $result['last_updated'] );
+
+		return $last_updated > $now->modify( '-2 years' );
+	}
+
+	/**
+	 * Determines whether a plugin has been tested with one of the three last major releases of WordPress.
+	 *
+	 * @param string $url URL to the plugin in the WordPress Plugin Directory.
+	 *
+	 * @return bool True if the plugin has been tested with recent WordPress major releases or not.
+	 */
+	protected function hasPluginBeenTestedWithLatestVersions( $url ) {
+		$result = $this->loadPluginData( $url );
+
+		if ( null === $result ) {
+			return false;
+		}
+
+		$tested     = $this->incrementVersion( $result['tested'], 3 );
+		$wp_version = $this->getLatestWordPressVersion();
+
+		return version_compare( $tested, $wp_version, '<' );
+	}
+
+	/**
+	 * Increment a version number a few times.
+	 *
+	 * Turns 1.2.3 into 1.4. or 1.5, for example.
+	 *
+	 * @param string $version Version number, e.g. 1.2.3
+	 * @param int $steps Number of steps the version number should be incremented.
+	 * @return string Incremented version number.
+	 */
+	protected function incrementVersion( $version, $steps = 1 ) {
+		while ( $steps > 0 ) {
+			$parts = explode( '.', $version );
+			$parts = array_splice( $parts, 0, 2 );
+
+			if ( $parts[1] + 1 < 10 ) {
+				$parts[1]++;
+			} else {
+				$parts[1] = 0;
+				$parts[0]++;
+			}
+
+			$version = implode( '.', $parts );
+
+			$steps --;
+		}
+
+		return $version;
+	}
+
+	/**
+	 * Returns the current WordPress version number.
+	 *
+	 * @return string Version number of the latest WordPress major release, e.g. 4.9
+	 */
+	protected function getLatestWordPressVersion() {
+		$result = file_get_contents( 'https://api.wordpress.org/core/version-check/1.7/' );
+
+		$result = json_decode( $result, true );
+
+		if ( isset( $result['offers'][0]['current'] ) ) {
+			$parts = explode( '.', $result['offers'][0]['current'] );
+			$parts = array_splice( $parts, 0, 2 );
+
+			return implode( '.', $parts );
+		}
+
+		return null;
 	}
 
 	/**
